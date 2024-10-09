e1e6fc9c425e67df716926239a64ed1bf6631a96
nakamura <hrm.nkmr04@gmail.com>
2024-10-10 08:04:16 +0900

    sixth commit

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -5,10 +5,11 @@ $(function () {
   function displayBooks(books) {
     $(".message").remove();
     $(".lists").empty();
-  
+
     // デバッグ用のログ
-    console.log(books); 
-  
+    console.log(books);
+
+    // itemsが存在しない場合の処理をelseのみにする
     if (books.items && books.items.length > 0) {
       $.each(books.items, function (index, book) {
         // 各bookオブジェクトを確認
@@ -32,21 +33,24 @@ $(function () {
         $(".lists").append(bookItem); // 要素を最後に追加
       });
     } else {
-      $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
-    }
+      $(".lists").before('<div class="message">\u691c\u7d22\u7d50\u679c\u304c\u898b\u3064\u304b\u308a\u307e\u305b\u3093\u3067\u3057\u305f\u3002<br>\u5225\u306e\u30ad\u30fc\u30ef\u30fc\u30c9\u3067\u691c\u7d22\u3057\u3066\u4e0b\u3055\u3044\u3002</div>');
+    }    
   }
 
   function displayError(err) {
     $(".lists").empty();
     $(".message").remove();
-    if (err.status === 0) {
-      $(".lists").before('<div class="message">正常に通信できませんでした。<br>インターネットの接続を確認してください。</div>');
-    } else if (err.status === 400) {
-      $(".lists").before('<div class="message">検索キーワードが有効ではありません。<br>1文字以上で検索してください。</div>');
-    } else {
-      $(".lists").before('<div class="message">予期せぬエラーが起きました。<br>再読み込みを行ってください。</div>');
-    }
-  }
+    
+    const errorMessages = {
+        0: '正常に通信できませんでした。<br>インターネットの接続を確認してください。',
+        400: '検索キーワードが有効ではありません。<br>1文字以上で検索してください。',
+        default: '予期せぬエラーが起きました。<br>再読み込みを行ってください。'
+    };
+    
+    const message = errorMessages[err.status] || errorMessages.default;
+    $(".lists").before(`<div class="message">${message}</div>`);
+}
+
 
   function getBooks(searchWord, pageCount) {
     const settings = {
@@ -55,10 +59,11 @@ $(function () {
     };
 
     $.ajax(settings)
-      .done(function (response) {
-        const result = response["@graph"][0];
-        displayBooks(result); 
-      })
+    .done(function (response) {
+      const result = response["@graph"][0]; // @graphの最初の要素
+      displayBooks(result);  // 直接呼び出し
+    })
+  
       .fail(function (err) {
         displayError(err);
       });
