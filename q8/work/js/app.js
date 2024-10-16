e5d067f32f73056499f17f91125393b19206dc93
nakamura <hrm.nkmr04@gmail.com>
2024-10-17 07:52:28 +0900

    seventh commit

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -4,7 +4,6 @@ $(function () {
 
   function displayBooks(books) {
     $(".message").remove();
-    $(".lists").empty();
 
     // デバッグ用のログ
     console.log(books);
@@ -33,24 +32,29 @@ $(function () {
         $(".lists").append(bookItem); // 要素を最後に追加
       });
     } else {
-      $(".lists").before('<div class="message">\u691c\u7d22\u7d50\u679c\u304c\u898b\u3064\u304b\u308a\u307e\u305b\u3093\u3067\u3057\u305f\u3002<br>\u5225\u306e\u30ad\u30fc\u30ef\u30fc\u30c9\u3067\u691c\u7d22\u3057\u3066\u4e0b\u3055\u3044\u3002</div>');
-    }    
+      $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してください。</div>');
+    }
   }
 
   function displayError(err) {
-    $(".lists").empty();
     $(".message").remove();
     
-    const errorMessages = {
-        0: '正常に通信できませんでした。<br>インターネットの接続を確認してください。',
-        400: '検索キーワードが有効ではありません。<br>1文字以上で検索してください。',
-        default: '予期せぬエラーが起きました。<br>再読み込みを行ってください。'
-    };
-    
-    const message = errorMessages[err.status] || errorMessages.default;
+    let message = '';
+  
+    switch (err.status) {
+      case 0:
+        message = '正常に通信できませんでした。<br>インターネットの接続を確認してください。';
+        break;
+      case 400:
+        message = '検索キーワードが有効ではありません。<br>1文字以上で検索してください。';
+        break;
+      default:
+        message = '予期せぬエラーが起きました。<br>再読み込みを行ってください。';
+    }
+  
     $(".lists").before(`<div class="message">${message}</div>`);
-}
-
+  }
+  
 
   function getBooks(searchWord, pageCount) {
     const settings = {
@@ -63,17 +67,16 @@ $(function () {
       const result = response["@graph"][0]; // @graphの最初の要素
       displayBooks(result);  // 直接呼び出し
     })
-  
-      .fail(function (err) {
-        displayError(err);
-      });
+    .fail(function (err) {
+      displayError(err);
+    });
   }
 
   $(".search-btn").on("click", function () {
     const newSearchWord = $("#search-input").val();
     if (newSearchWord !== searchWord) {
       pageCount = 1;
-      $(".lists").empty();
+      $(".lists").empty(); // 新しい検索ワードの場合のみ結果をクリア
       searchWord = newSearchWord;
     } else {
       pageCount++;
