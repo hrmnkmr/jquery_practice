098d732731e276664dcd3aeb5496511f6b358def
nakamura <hrm.nkmr04@gmail.com>
2025-02-18 19:54:05 +0900

    fourth commit

--- q8/work/js/app.js
+++ q8/work/js/app.js
@@ -5,16 +5,18 @@ $(function () {
 
   // 検索結果を表示する関数
   function displayResult(result) {
-    if (result.length > 0) {
-      // 結果を降順に並べ替え
-      result.reverse();  // 降順にする
-      
+    console.log("受け取ったデータ:", result); // 受け取ったデータ全体を確認
+
+    if (result && result.length > 0) { // データがあるか確認
       $.each(result, function (index, item) {
+        console.log("処理中のアイテム:", item); // 各アイテムのデータを確認
+
         var html = '<li class="lists-item"><div class="list-inner"><p>タイトル：' + (item.title || "タイトル不明") + '</p>' +
           '<p>作者：' + (item["dc:creator"] || "作者不明") + '</p>' +
           '<p>出版社：' + (item["dc:publisher"] || "出版社不明") + '</p>' +
-          '<a href="' + item.link["@id"] + '" target="_blank">書籍情報</a></div></li>';
-        $(".lists").prepend(html); // 結果をリストの上に追加
+          '<a href="' + (item.link?.["@id"] || "#") + '" target="_blank">書籍情報</a></div></li>';
+
+        $(".lists").prepend(html);
       });
     } else {
       $(".lists").before('<div class="message">検索結果が見つかりませんでした。<br>別のキーワードで検索してみてください。</div>');
@@ -35,7 +37,7 @@ $(function () {
         url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
         method: "GET"
       }).done(function (response) {
-        displayResult(response["@graph"]); // 結果を表示
+        displayResult(response["@graph"][0].items); // 修正
       }).fail(function (err) {
         $(".lists").empty();
         $(".message").remove();
@@ -60,7 +62,7 @@ $(function () {
       url: `https://ci.nii.ac.jp/books/opensearch/search?title=${searchWord}&format=json&p=${pageCount}&count=20`,
       method: "GET"
     }).done(function (response) {
-      displayResult(response["@graph"]); // 結果をリストの上に追加
+      displayResult(response["@graph"][0].items); // 修正
     }).fail(function (err) {
       $(".lists").before('<div class="message">通信に失敗しました。再度お試しください。</div>');
     });
